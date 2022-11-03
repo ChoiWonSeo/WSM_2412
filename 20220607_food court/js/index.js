1cf0c2b1e7dcac5d91d1a7cfa99720e754b695c6
Choi Won Seo <cws4725@gmail.com>
2022-11-03 15:10:52 +0900

    js 오늘의 메뉴 급식 api에서 가져와서 html에 표시

--- 20220607_food court/js/index.js
+++ 20220607_food court/js/index.js
@@ -81,6 +81,77 @@ showToday();
 
 // 오늘의 급식 가져오고, 표시하자
 const showTodayMenu = () => {
+    // 지금 구하자
+    // 년, 월, 일 구하자
+    let now = new Date();
+    let year = now.getFullYear();
+    let month = now.getMonth() + 1; // 0 ~ 11
+    let date = now.getDate();
+
+    // 급식API 요청할 url 만들자
+    const KEY = "a9c902f701a34fe494d123a5b4970beb";
+    const ATPT_OFCDC_SC_CODE = "B10";   // 서울특별시교육청
+    const SD_SCHUL_CODE = "7010569";    // 미림여자정보과학고등학교
+
+    let MLSV_YMD = `${year}${month.toString().padStart(2, "0")}${date.toString().padStart(2, "0")}`; 
+    console.log(MLSV_YMD);
+    const Type = "json";
+    let url = `https://open.neis.go.kr/hub/mealServiceDietInfo`
+            + `?KEY=${KEY}`
+            + `&ATPT_OFCDC_SC_CODE=${ATPT_OFCDC_SC_CODE}`
+            + `&SD_SCHUL_CODE=${SD_SCHUL_CODE}`
+            // + `&MMEAL_SC_CODE=${MMEAL_SC_CODE}`
+            + `&MLSV_YMD=${MLSV_YMD}`
+            + `&Type=json`;
+
+    console.log(url);
+
+    // 요청하자
+    fetch(url)
+    .then(response => response.json())
+    .then(json => console.log(json)); // json -> HTML에 표시하자
+};
+const showMenu = (json) => {
+    // html -> js 메뉴 표시하는 부분
+    let menus = document.querySelectorAll(".card-menu");
+    let breakfast = menus[0];
+    let lunch = menus[1];
+    let dinner = menus[2];
+    // json 안에 조식, 중식, 석식 정보 빼오고
+    try {
+        if (json['mealServiceDietInfo'][0]['head'][1]['RESULT']['CODE'] == 'INFO-000') {
+            // 응답이 제대로 왔으면
+            // json -> HTML
+            // 하나하나 하고싶으면 아래 애들 하나하나 try catch문으로 넣어줘야함 ㅋㅋ
+            let breakfastData = json['mealServiceDietInfo'][1]['row'][0]['DDISH_NM'];
+            // (5.13.) << 삭제하기   정규표현식 : (문자 숫자나 .문자)문자
+            breakfastData = breakfastData.replace(/\([0-9\.]*\)/g, "");
+            breakfast.innerHTML = breakfastData;
+
+            let lunchData = json['mealServiceDietInfo'][1]['row'][1]['DDISH_NM'];
+            lunchData = lunchData.replace(/\([0-9\.]*\)/g, "");
+            lunch.innerHTML = lunchData;
+
+            let dinnerData = json['mealServiceDietInfo'][1]['row'][2]['DDISH_NM'];
+            dinnerData = dinnerData.replace(/\([0-9\.]*\)/g, "");
+            dinner.innerHTML = dinnerData;
+
+            // (    \(
+            // 숫자 한글자
+        } else {
+            // 응답이 이상하면
+            // 없음 표시하자
+            breakfast.innerHTML = "없음";
+            lunch.innerHTML = "없음";
+            dinner.innerHTML = "없음";
+        }
+    } catch { // 문제가 생기면 {'RESULT':}
+        breakfast.innerHTML = "없음";
+        lunch.innerHTML = "없음";
+        dinner.innerHTML = "없음";
+    }
+    // HTML에 표시하자
 
 }
+
 showTodayMenu();
